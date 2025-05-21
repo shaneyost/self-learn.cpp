06cc0fd55c86ee549c4675bdd90392f96e064572
Shane Yost <scy@shaneyost.io>
2025-05-21 17:47:00 -0600

    Learned about strtol vs stoi, advantages, disadvantages

--- env_variables/main.cpp
+++ env_variables/main.cpp
@@ -48,5 +48,73 @@ int main ()
     {
         std::cout << "KITTY_PID is not set" << std::endl;
     }
+
+    // Just read through the docs and std::stoi takes a const std::string&. However, I get away
+    // with passing a const char* because of a implicit conversion happening. A maybe cleaner more
+    // explicit way to write it would be std::stoi(std::string(value))
+
+    // Another reading through some docs concluded that the conversion from const char* to the
+    // const std::string& does allocate a temporary string on the heap. For high performance or 
+    // tight loops I might want to think about strtol. 
+
+    // For a brief moment I starting thinking "if strtol doesn't give me exception based error
+    // handling then why not just use atoi()?". After some more reading I found out that strtol
+    // provides error detection specifically for underflow and overflow which is more than what
+    // atoi() provides. Also strtol can error detect and handle invalid input which atoi doesn't. 
+    // There's also more options in turns of return types than what atoi can provide too.
+    
+    const char* avalue = std::getenv("KITTY_PID");
+    if (value)
+    {
+        try
+        {
+            int value_to_integer = std::stoi(std::string(avalue));
+            std::cout << value_to_integer << std::endl;
+        }
+        catch (const std::invalid_argument)
+        {
+            std::cerr << "KITTY_PID is not a valid number" << std::endl;
+        }
+        catch (const std::out_of_range)
+        {
+            std::cerr << "KITTY_PID is out of integer range" << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "KITTY_PIT is not set" << std::endl;
+    }
+
+    // So lets say we want to use strtol, what would that look like? I don't like the fact I have
+    // to write out long int. 
+    //
+    // I could use smaller type like int but I risk truncation and I would have to manually check
+    // for that which I also don't want to do. So it would be better to just uint64_t or write out
+    // long it.
+    
+    const char* bvalue = std::getenv("KITTY_PID");
+    if (value)
+    {
+        char* end;
+        errno = 0;
+        long int value_to_integer = std::strtol(bvalue, &end, 10);
+
+        if (errno == ERANGE)
+        {
+            std::cerr << "KITTY_PID is out of range" << std::endl;
+        }
+        else if (end == value || *end != '\0')
+        {
+            std::cerr << "KITTY_PID is not a valid integer" << std::endl;
+        }
+        else
+        {
+            std::cout << "KITTY_PID: " << value_to_integer << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "KITTY_PID is not set" << std::endl;
+    }
     return 0;
 }
